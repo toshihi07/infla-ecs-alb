da1f849a5b0eb239f3cdc503eecc1278f41509aa
root <root@shimmachi>
2025-05-14 16:07:40 +0900

    dynamodb-table-items-create

---
+++ terraform/dynamodb.tf
@@ -0,0 +1,16 @@
+resource "aws_dynamodb_table" "items" {
+  name         = "items"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "items"
+    Environment = "dev"
+  }
+}
+
